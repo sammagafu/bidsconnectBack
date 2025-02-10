16fdea26338ec2b2c19d105ab98e2fc18080e1e6
Samuel Magafu <hi@samuelmagafu.com>
2025-02-10 14:25:59 +0300

    added apps to the project

--- bidsconnect/settings.py
+++ bidsconnect/settings.py
@@ -37,6 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # apps
+    'accounts',
+    'analytics',
+    'bids',
+    'payments',
+    'tenders',
+
+    # third party apps
+    'rest_framework',
+    'djoser',
+
 ]
 
 MIDDLEWARE = [
